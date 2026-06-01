48e298cd2656c2b49ae2123f8ab337d4210b5fa0
caicedochristopher2-crypto <caicedochristopher2@gmail.com>
2026-06-01 12:41:56 -0500

    Update Docente.java

--- src/expo/Docente.java
+++ src/expo/Docente.java
@@ -8,6 +8,41 @@ package expo;
  *
  * @author TONY_VERA
  */
-public class Docente {
-    
+public class Docente extends Usuario {
+
+    private String departamento;
+    private String especialidad;
+
+    public Docente() {
+    }
+
+    public Docente(String departamento, String especialidad) {
+        this.departamento = departamento;
+        this.especialidad = especialidad;
+    }
+
+    public Docente(String departamento, String especialidad, int id, int cedula, String nombre, int edad, String direccionDomicilio) {
+        super(id, cedula, nombre, edad, direccionDomicilio);
+        this.departamento = departamento;
+        this.especialidad = especialidad;
+    }
+
+  
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
 }
+
