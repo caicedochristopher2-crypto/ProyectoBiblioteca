238d08f47174479c2b3f437a6fd58dd741e98ab8
caicedochristopher2-crypto <caicedochristopher2@gmail.com>
2026-06-01 12:25:14 -0500

    Update Prestamo.java

--- src/expo/Prestamo.java
+++ src/expo/Prestamo.java
@@ -8,10 +8,98 @@ package expo;
  *
  * @author TONY_VERA
  */
-public class Prestamo {
-    //ATIBUTOS
+
     private String usuario,material,fechaPrestamo,fechaEntrega;
     private double multa;
+
+    public Prestamo() {
+    }
+
+    public Prestamo(String usuario, String material, String fechaPrestamo, String fechaEntrega, double multa) {
+        this.usuario = usuario;
+        this.material = material;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaEntrega = fechaEntrega;
+        this.multa = multa;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public String getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(String fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public double getMulta() {
+        return multa;
+    }
+
+    public void setMulta(double multa) {
+        this.multa = multa;
+    }
     
+    @Override
+    public void prestar() {
+        try {
+        if (material == null) {
+            throw new Exception("Material no disponible");
+        }
+
+        System.out.println("Préstamo realizado a: " + usuario);
+
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+       
+    }
+
+    @Override
+public void devolver() {
+    int diasRetraso = 5; 
+
+    if (material.equalsIgnoreCase("libro")) {
+        multa = diasRetraso * 0.5;
+    } else if (material.equalsIgnoreCase("revista")) {
+        multa = diasRetraso * 1.0;
+    } else {
+        multa = diasRetraso * 1.5;
+    }
+
+    System.out.println("Devuelto por: " + usuario);
+    System.out.println("Multa: $" + multa);
+}
+    
+    
+    public void mostrarPrestamo() {
+        System.out.println("Usuario: " + usuario);
+        System.out.println("Material: " + material);
+        System.out.println("Fecha préstamo: " + fechaPrestamo);
+        System.out.println("Multa: $" + multa);
+    }
     
 }
